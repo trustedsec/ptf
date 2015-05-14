a966f19738222001d8b5396edac9c68a4c8eae5c
l0gan <kirk.paul.hayes+github@gmail.com>
2015-05-14 09:34:28 -0400

    Add in EyeWitness

---
+++ modules/intelligence-gathering/eyewitness.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#######################################
+# Installation module for dictionaries
+#######################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Kirk Hayes (l0gan)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update EyeWitness."
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/ChrisTruncer/EyeWitness"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="eyewitness"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN=""
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS="cd {INSTALL_LOCATION},setup/setup.sh,exit"
