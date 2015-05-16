3db108b8cd25c73e59be9ea024be71c31409042c
Tim Fowler <tim@roobixx.com>
2015-05-15 21:36:26 -0400

    Create Wifite Module

---
+++ modules/wireless/wifite.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for Wifite
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Tim Fowler (roobixx)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update wifite"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/derv82/wifite" 
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="wifite"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN=""
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
