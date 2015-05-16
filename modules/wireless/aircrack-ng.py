1bfa01a18c4603567c283f66ba0fef3524d54838
root <root@localhost.localdomain>
2015-05-15 21:24:08 -0400

    correct dependencies

--- modules/wireless/aircrack-ng.py
+++ modules/wireless/aircrack-ng.py
@@ -7,20 +7,20 @@
 AUTHOR="Tim Fowler (roobixx)"
 
 # DESCRIPTION OF THE MODULE
-DESCRIPTION="This module will install/update "
+DESCRIPTION="This module will install/update aircrack-ng (without "
 
 # INSTALL TYPE GIT, SVN, FILE DOWNLOAD
 # OPTIONS = GIT, SVN, FILE
 INSTALL_TYPE="GIT"
 
 # LOCATION OF THE FILE OR GIT/SVN REPOSITORY
-REPOSITORY_LOCATION="https://github.com/aircrack-ng/aircrack-ng"
+REPOSITORY_LOCATION="https://github.com/aircrack-ng/aircrack-ng" 
 
 # WHERE DO YOU WANT TO INSTALL IT
 INSTALL_LOCATION="aircrack-ng"
 
 # DEPENDS FOR DEBIAN INSTALLS
-DEBIAN="libsqlite3-devel"
+DEBIAN="libsqlite3-devel,libnl-dev"
 
 # COMMANDS TO RUN AFTER
-AFTER_COMMANDS=""cd {INSTALL_LOCATION},make,make strip, make install,exit""
+AFTER_COMMANDS=""cd {INSTALL_LOCATION},make,make strip,make install,exit""
