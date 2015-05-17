43dbd55282cfaffd8338c9cba06e69a320e2a38e
root <info@trustedsec.com>
2015-05-17 09:51:22 -0400

    Updated description for discover

--- modules/intelligence-gathering/discover.py
+++ modules/intelligence-gathering/discover.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 #####################################
-# Installation module for smbexec
+# Installation module for discover
 #####################################
 
 # AUTHOR OF MODULE NAME
 AUTHOR="Martin Bos (purehate)"
 
 # DESCRIPTION OF THE MODULE
-DESCRIPTION="This module will install/update "
+DESCRIPTION="This module will install/update discover - an OSINT tool"
 
 # INSTALL TYPE GIT, SVN, FILE DOWNLOAD
 # OPTIONS = GIT, SVN, FILE
