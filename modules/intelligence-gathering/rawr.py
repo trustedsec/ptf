a64e1deda6c78b15608a1b99c2617a152ea83a37
Justin <Justin6@gmail.com>
2015-06-22 10:05:47 -0400

    added rawr.py

---
+++ modules/intelligence-gathering/rawr.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#######################################
+# Installation module for RAWR
+#######################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Justin Herman (JDogHerman)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update RAWR - Rapid Assessment of Web Resources."
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://bitbucket.org/al14s/rawr.git"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="rawr"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN=""
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS="cd {INSTALL_LOCATION},./install.sh y"
