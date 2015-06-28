b5b0edaf9af0924ee79c5c1e64a1dc18c259d091
Mauro Risonho de Paula Assumpcao <mrpa.security@gmail.com>
2015-06-28 16:35:54 -0300

    Create theHarvester.py
    Initial file theHarvester.py

---
+++ modules/intelligence-gathering/theHarvester.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for theHarvester
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Mauro Risonho de Paula Assumpcao (firebits)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update E-mail, subdomain and people names harvester"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/laramies/theHarvester.git"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="theHarvester"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="python"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS="cd {INSTALL_LOCATION}"
