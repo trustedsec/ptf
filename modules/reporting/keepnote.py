21411b53ecb42b4133267f4f8bd24aafcf297af9
Mauro Risonho de Paula Assumpcao <mrpa.security@gmail.com>
2015-06-28 16:09:17 -0300

    Create keepnote.py
    Initial file keepnote.py

---
+++ modules/reporting/keepnote.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#######################################
+# Installation module for Keepnote
+#######################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Mauro Risonho de Paula Assumpcao (firebits)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update "
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/mdrasmus/keepnote.git"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="keepnote"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="python-gnome2-extras,aspell,aspell-en,python,python-gtk2,python-glade2,libgtk2.0-dev,libsqlite3-0"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS="cd {INSTALL_LOCATION},python setup.py install --prefix={INSTALL_LOCATION}"
