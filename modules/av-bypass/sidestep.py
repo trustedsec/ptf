f5792969032a544c3e79db7996b485827ce6b5c7
TrustedSec <davek@trustedsec.com>
2015-07-19 21:17:06 -0400

    removed veil, added sidestep

--- modules/av-bypass/sidestep.py
+++ modules/av-bypass/sidestep.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 #####################################
-# Installation module for veil
+# Installation module for SideStep
 #####################################
 
 # AUTHOR OF MODULE NAME
-AUTHOR="Justin Herman (JDogHerman)"
+AUTHOR="David Kennedy (HackingDave)"
 
 # DESCRIPTION OF THE MODULE
-DESCRIPTION="This module will install/update veil"
+DESCRIPTION="This module will install/update sidestep - a Metasploit AV evasion"
 
 # INSTALL TYPE GIT, SVN, FILE DOWNLOAD
 # OPTIONS = GIT, SVN, FILE
 INSTALL_TYPE="GIT"
 
 # LOCATION OF THE FILE OR GIT/SVN REPOSITORY
-REPOSITORY_LOCATION="https://github.com/Veil-Framework/Veil"
+REPOSITORY_LOCATION="https://github.com/codewatchorg/sidestep"
 
 # WHERE DO YOU WANT TO INSTALL IT
-INSTALL_LOCATION="veil"
+INSTALL_LOCATION="sidestep"
 
 # DEPENDS FOR DEBIAN INSTALLS
 DEBIAN=""
 
 # COMMANDS TO RUN AFTER
-AFTER_COMMANDS="cd {INSTALL_LOCATION},export DEBIAN_FRONTEND=noninteractive,echo y | ./Install.sh -c"
+AFTER_COMMANDS=""
