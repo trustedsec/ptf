4162fafe7c41e206591cf90a21a85d51b9e111fe
TrustedSec <davek@trustedsec.com>
2015-09-14 15:24:22 -0400

    Update to add dnsenum

---
+++ modules/intelligence-gathering/dnsenum.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for DNSEnum
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Dave Kennedy (HackingDave)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update dnsenum - a DNS scanning tool"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/fwaeytens/dnsenum"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="dnsenum"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="perl"
+
+# COMMANDS TO RUN AFTER 
+AFTER_COMMANDS="export PERL_MM_USE_DEFAULT=1,export PERL_EXTUTILS_AUTOINSTALL='--defaultdeps',cpan -fi Net::IP Net::DNS Net::Netmask Net::Whois::IP HTML::Parser WWW::Mechanize XML::Writer String::Random"
+
+# create a launcher
+LAUNCHER="dnsenum"
