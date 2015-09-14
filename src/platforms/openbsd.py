859d29d29e2da0d7e1182e053e9ed3868b63013e
Mauro Risonho de Paula Assumpcao <mrpa.security@gmail.com>
2015-09-14 17:39:14 -0300

    Create openbsd.py
    Update packages openbsd

---
+++ src/platforms/openbsd.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+########################################
+# Core installation for OpenBSD Packages
+########################################
+from src.core import logging
+import subprocess
+
+# this will do updates and installations
+def base_install_modules(module_name):
+    
+    # will work for 1 or more space- or comma-separated modules
+    modules = module_name.replace(",", " ")
+    command = "pkg_add -Uu && pkg_add -ui && pkg_add -uvi " + modules
+    subprocess.Popen(command, shell=True).wait()
