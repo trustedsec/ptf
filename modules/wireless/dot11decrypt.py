2d6300b36c597706bd542c3a22ff323c59650cbe
João Pena Gil <Jack64@users.noreply.github.com>
2015-10-15 16:38:41 +0100

    Create dot11decrypt.py

---
+++ modules/wireless/dot11decrypt.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for dot11decrypt
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Joao Pena Gil (Jack64)"
+
+# DESCRIPTION OF THE MODULE  
+DESCRIPTION="This module will install/update dot11decrypt, a WEP/WPA2(AES and TKIP) on-the-fly decrypter."
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/mfontanini/dot11decrypt/"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="dot11decrypt"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="libtins1,libtins2,libtins-dev"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA=""
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS="cd {INSTALL_LOCATION},./configure,make,make install"
