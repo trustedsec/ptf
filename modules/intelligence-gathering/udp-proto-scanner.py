33d5f62ac2dda431a028c64893b8452083868b4f
Steven van der Baan <steven@pentest.7safe.com>
2015-11-05 21:38:37 +0000

    Added two new modules: - udb-proto-scanner - whatweb
    Signed-off-by: Steven van der Baan <steven@pentest.7safe.com>

---
+++ modules/intelligence-gathering/udp-proto-scanner.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for discover
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Steven van der Baan (vdbaan)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update UDP-Proto-scanner"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/portcullislabs/udp-proto-scanner.git"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="udpprotoscanner"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
