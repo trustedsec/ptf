179f6240555550e372a251059abb2c02a4c1e609
TrustedSec <davek@trustedsec.com>
2015-11-06 03:01:00 -0500

    Added kismet wireless

---
+++ modules/wireless/kismet.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for kismet
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Dave Kennedy (HackingDave)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update kismet, a tool for wireless auditing"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://www.kismetwireless.net/kismet.git" 
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="kismet"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS="cd {INSTALL_LOCATION},./configure, make,make install"
