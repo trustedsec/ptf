880982a853b7f8d627bb48e96d02c691a7a0a117
TrustedSec <info@trustedsec.com>
2016-09-06 14:33:28 -0400

    add generate macro

---
+++ modules/powershell/generatemacro.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for Generate-Macro
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="David Kennedy (ReL1K)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This Powershell script will generate a malicious Microsoft Office document with a specified payload and persistence method."
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/enigma0x3/Generate-Macro"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="generate-macro"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
+# THIS WILL CREATE AN AUTOMATIC LAUNCHER FOR THE TOOL
+LAUNCHER=""
+
