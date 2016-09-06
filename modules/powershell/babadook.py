39b73e1ace459e98fe8c9e8cf094eb4b42ca3ec0
TrustedSec <info@trustedsec.com>
2016-09-06 14:28:23 -0400

    add babadook

---
+++ modules/powershell/babadook.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for BABADOOK
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="David Kennedy (ReL1K)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update Connection-less Powershell Persistent and Resilient Backdoor"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/jseidl/Babadook/"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="babadook"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
+# THIS WILL CREATE AN AUTOMATIC LAUNCHER FOR THE TOOL
+LAUNCHER=""
+
