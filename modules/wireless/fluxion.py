537e23602799d056ada1560a79ec50ad990f3cbb
bustedsec <frank.trezza@gmail.com>
2017-04-13 19:15:02 -0400

    Fixed Dirb Installer - needed to modify framework.py and switch the order of two lines in order for this fix to work. Fix sugguested by oliverloveless "The issue is that ptf makes assumptions on whether the repo needs to be compiled before a launcher can be created.
    ptf assumes that all git repos are not compiled, and therefore thinks it's ok to create the launcher, before the after_commands (as specified in the config file) are run. This causes an issue in any compiled tool originating from github, because they don't have the appropriately named executable file in the repository (which src.core.launcher() uses in the launcher creation process)."
    
    Fixed Fluxion installer - deltaflux took his repo offline due to laws in his country but put a copy up on his website which this installer now grabs.
    
    Permissons on msfdb.sh automatically changed during program execution - I did not manually  chmod this -  it happened after running the installer - but the updated permissions are carrying over to this commit

--- modules/wireless/fluxion.py
+++ modules/wireless/fluxion.py
@@ -4,28 +4,28 @@
 #####################################
 
 # AUTHOR OF MODULE NAME
-AUTHOR="Frank Trezza (thatrez)"
+AUTHOR="Frank Trezza (bustedsec)"
 
 # DESCRIPTION OF THE MODUL
 DESCRIPTION="This module will install/update fluxion - a fake access point designed to trick users into divulgining wifi passwords"
 
 # INSTALL TYPE GIT, SVN, FILE DOWNLOAD
 # OPTIONS = GIT, SVN, FILE
-INSTALL_TYPE="GIT"
+INSTALL_TYPE="FILE"
 
 # LOCATION OF THE FILE OR GIT/SVN REPOSITORY
-REPOSITORY_LOCATION="https://github.com/deltaxflux/fluxion" 
+REPOSITORY_LOCATION="https://fluxion.tk/fluxion-unstable.zip" 
 
 # WHERE DO YOU WANT TO INSTALL IT
 INSTALL_LOCATION="fluxion"
 
 # DEPENDS FOR DEBIAN INSTALLS
-DEBIAN="bully"
+DEBIAN="bully, unzip"
 
 # DEPENDS FOR FEDORA INSTALLS
-FEDORA="git"
+FEDORA="git, unzip"
 
 # COMMANDS TO RUN AFTER
-AFTER_COMMANDS="cd {INSTALL_LOCATION},bash Installer.sh"
+AFTER_COMMANDS="cd {INSTALL_LOCATION},unzip fluxion-unstable.zip, fluxion/fluxion-master/install/install.sh"
 
 LAUNCHER="fluxion"
