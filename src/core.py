5a74edf0811edd54b9133e5dbf9f7c1f6782e1ba
Alex Flores <domain@me.com>
2017-06-08 00:59:12 -0400

    changing cwd before using tool has adverse effects

--- src/core.py
+++ src/core.py
@@ -380,8 +380,8 @@ def launcher(filename, install_location):
                 # if we found filetype
                 if point != "":
                     filewrite = open("/usr/local/bin/" + launchers, "w")
-                    filewrite.write('#!/bin/sh\ncd %s\nfind %s -executable || chmod +x %s\n%s $*\n' %
-                                    (install_location, file_point, file_point, point))
+                    filewrite.write('#!/bin/sh\n[ -e %s%s ] || chmod +x %s%s\n%s%s $*\n' %
+                                    (install_location, file_point, install_location, file_point, install_location, file_point))
                     filewrite.close()
                     subprocess.Popen("chmod +x /usr/local/bin/%s" %
                                      (launchers), shell=True).wait()
