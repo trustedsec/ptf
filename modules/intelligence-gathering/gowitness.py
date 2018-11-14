f1c7274ab788b8069c5db22e73436f663c45906f
Zawadi Done <21525407+Zawadidone@users.noreply.github.com>
2018-11-14 23:52:15 +0000

    Update gowitness.py

--- modules/intelligence-gathering/gowitness.py
+++ modules/intelligence-gathering/gowitness.py
@@ -10,7 +10,7 @@ INSTALL_LOCATION="gowitness"
 
 DEBIAN="golang" 
 
-AFTER_COMMANDS="cd {INSTALL_LOCATION},go get -v -u github.com/golang/dep/cmd/dep,cd ~/go/src, git clone https://github.com/sensepost/gowitness, cd gowitness,dep ensure, go build, mv gowitness /usr/local/bin/, wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb, dpkg -i google-chrome-stable_current_amd64.deb, apt install -f, dpkg -i google-chrome-stable_current_amd64.deb, rm *.deb"
+AFTER_COMMANDS="cd {INSTALL_LOCATION},go get -v -u github.com/golang/dep/cmd/dep,cd ~/go/src, git clone https://github.com/sensepost/gowitness, cd gowitness,dep ensure, go build, cp gowitness /usr/local/bin/, wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb, dpkg -i google-chrome-stable_current_amd64.deb, apt install -f, dpkg -i google-chrome-stable_current_amd64.deb, rm *.deb"
 
 LAUNCHER=""
 
