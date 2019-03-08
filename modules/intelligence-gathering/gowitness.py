7c1c7b7f53e92498c6bc2f3d68a8587e8957c871
Zawadi Done <hi@zawadidone.nl>
2019-03-08 23:33:49 +0100

    Update gowitness.py

--- modules/intelligence-gathering/gowitness.py
+++ modules/intelligence-gathering/gowitness.py
@@ -16,11 +16,11 @@ INSTALL_TYPE="GIT"
 REPOSITORY_LOCATION="https://github.com/sensepost/gowitness" 
 
 
-# WHERE DO YOU WANT TO INSTALL IT
+# WHERE DO YOU WANT TO INSTALL ITexi
 INSTALL_LOCATION="gowitness"
 
 # DEPENDS FOR DEBIAN INSTALLS
-DEBIAN="golang" 
+DEBIAN="golang,wget,curl" 
 
 # COMMANDS TO RUN AFTER
-AFTER_COMMANDS="echo 'export GOPATH=$HOME/go' > ~/.profile, . ~/.profile,go get -u github.com/golang/dep/cmd/dep,cp ~/go/bin/dep /usr/local/bin/,cd /root/go/src,git clone https://github.com/sensepost/gowitness .,cd gowitness,dep ensure -update,make,cd build,cp gowitness-linux-amd64 gowitness,cp /root/go/src/gowitness/build/gowitness /usr/local/bin/,cd {INSTALL_LOCATION},wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb, dpkg -i google-chrome-stable_current_amd64.deb, apt install -f -y, dpkg -i google-chrome-stable_current_amd64.deb, rm google-chrome-stable_current_amd64.deb"
+AFTER_COMMANDS="echo 'export GOPATH=$HOME/go' > ~/.profile, . ~/.profile,cp ~/go/bin/dep /usr/local/bin,curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh,cd ~/go/src,cd {INSTALL_LOCATION},dep ensure -update,make,cd build,cp gowitness-linux-amd64 gowitness,cp gowitness /usr/local/bin/,cd {INSTALL_LOCATION},wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb, dpkg -i google-chrome-stable_current_amd64.deb, apt install -f -y, dpkg -i google-chrome-stable_current_amd64.deb, rm google-chrome-stable_current_amd64.deb"
