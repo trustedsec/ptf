260018db17e4e452be5e7d72e4a6b835c9ce24ef
devilok <chitchcock@gmail.com>
2019-06-04 23:01:32 +0000

    Adding LinkFinder to dig through .js files for endpoints

---
+++ modules/intelligence-gathering/linkfinder.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for LinkFinder
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="devilok"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update LinkFinder - a tool to find endpoints and their parameters in JavaScript files"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/GerbenJavado/LinkFinder"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="LinkFinder"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
+# THIS WILL CREATE AN AUTOMATIC LAUNCHER FOR THE TOOL
+LAUNCHER="linkfinder.py"
