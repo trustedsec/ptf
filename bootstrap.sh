f8c8338cc158a5aabbc06e37ffb6b71b02e78193
spinfoo <jacobo@defensahacker.com>
2019-08-29 18:45:35 +0200

    Fixed broken dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@
 echo "docker-ptf" > /etc/hostname
 apt-get update
 apt-get upgrade -y
-apt-get install -y python git sudo locate vim pip tcpdump net-tools #libgmp3-dev:i386
+apt-get install -y apt-utils wget python git sudo locate vim python-pip tcpdump net-tools flex
 cd /tmp
 wget http://http.kali.org/pool/main/u/unicornscan/unicornscan_0.4.7-1kali2_amd64.deb
 dpkg -i ./unicornscan_0.4.7-1kali2_amd64.deb
