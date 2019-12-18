trustedsec/ptf

commit a666341aadcaaf33bdafd0d612a5000a97378a0f

svn proc.wait()

src/framework.py
@@ -415,7 +415,7 @@ def use_module(module, all_trigger):
 
                         if install_type.lower() == "svn":
                             print_status("Updating the tool, be patient while svn pull is initiated.")
-                            subprocess.Popen("cd %s;svn update" % (install_location), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
+                            proc = subprocess.Popen("cd %s;svn update" % (install_location), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
                             # here we do some funky stuff to store old
                             # revisions
                             try:
@@ -434,6 +434,8 @@ def use_module(module, all_trigger):
                                         prompt = "goat"
                             except:
                                 pass
+                            finally:
+                                proc.wait()
                             print_status("Finished Installing! Enjoy the tool installed under: " + (install_location))
                             # check launcher
                             launcher(filename, install_location)

commit ada0891687b5a17678a47226f1e3cbdd635bdff4

fix bug introduced by in a666341

src/framework.py
@@ -390,7 +390,7 @@ def use_module(module, all_trigger):
                     if os.path.isdir(install_location):
                         if install_type.lower() == "git":
                             print_status("Updating the tool, be patient while git pull is initiated.")
-                            subprocess.Popen("cd %s;git pull" % (install_location), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
+                            proc = subprocess.Popen("cd %s;git pull" % (install_location), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
                             # check launcher
                             launcher(filename, install_location)