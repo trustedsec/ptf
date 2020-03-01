690dc3430ba5a87b0efa142d9603bc99e25eb7ee
SabeBarker <SabeBarker@users.noreply.github.com>
2020-03-01 16:58:20 +0800

    fixes trustedsec/ptf#486

--- modules/password-recovery/password_analysis_and_cracking_kit.py
+++ modules/password-recovery/password_analysis_and_cracking_kit.py
@@ -4,23 +4,23 @@
 #####################################
 
 # AUTHOR OF MODULE NAME
-AUTHOR="Larry Spohn (Spoonman)"
+AUTHOR="Sabe Barker"
 
 # DESCRIPTION OF THE MODULE
 DESCRIPTION="This module will install/update PACK - a collection of utilities designed for advanced cracking with hashcat"
 
 # INSTALL TYPE GIT, SVN, FILE DOWNLOAD
 # OPTIONS = GIT, SVN, FILE
-INSTALL_TYPE="FILE"
+INSTALL_TYPE="GIT"
 
 # LOCATION OF THE FILE OR GIT/SVN REPOSITORY
-REPOSITORY_LOCATION="http://thesprawl.org/media/projects/PACK-0.0.4.tar.gz"
+REPOSITORY_LOCATION="https://github.com/Hydraze/pack.git"
 
 # WHERE DO YOU WANT TO INSTALL IT
 INSTALL_LOCATION="pack"
 
 # DEPENDS FOR DEBIAN INSTALLS
-DEBIAN="libenchant1c2a"
+DEBIAN="libenchant1c2a,python3-enchant"
 
 # DEPENDS FOR FEDORA INSTALLS
 FEDORA=""
@@ -29,4 +29,4 @@ FEDORA=""
 BYPASS_UPDATE="NO"
 
 # COMMANDS TO RUN AFTER 
-AFTER_COMMANDS="cd {INSTALL_LOCATION},tar xzf PACK-0.0.4.tar.gz --strip 1,rm PACK-0.0.4.tar.gz"
+AFTER_COMMANDS="cd {INSTALL_LOCATION},ln -s {INSTALL_LOCATION}maskgen.py /usr/bin/maskgen,ln -s {INSTALL_LOCATION}policygen.py /usr/bin/policygen,ln -s {INSTALL_LOCATION}rulegen.py /usr/bin/rulegen,ln -s {INSTALL_LOCATION}statsgen.py /usr/bin/statsgen"
