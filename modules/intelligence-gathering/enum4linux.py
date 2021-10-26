46f3f7d753cb09b7c316860eee97d597b6813610
skapunker <skapunker@gmail.com>
2021-10-26 17:05:55 -0600

    Update repo, install, and update polenum for Enum4Linux

--- modules/intelligence-gathering/enum4linux.py
+++ modules/intelligence-gathering/enum4linux.py
@@ -4,17 +4,17 @@
 #####################################
 
 # AUTHOR OF MODULE NAME
-AUTHOR="Steven van der Baan (vdbaan)"
+AUTHOR="Corey Batiuk (skapunker)"
 
 # DESCRIPTION OF THE MODULE
-DESCRIPTION="This module will install/update enum4linux (Portcullis Labs)."
+DESCRIPTION="This module will install/update enum4linux."
 
 # INSTALL TYPE GIT, SVN, FILE DOWNLOAD
 # OPTIONS = GIT, SVN, FILE
 INSTALL_TYPE="GIT"
 
 # LOCATION OF THE FILE OR GIT/SVN REPOSITORY
-REPOSITORY_LOCATION="https://github.com/portcullislabs/enum4linux.git"
+REPOSITORY_LOCATION="https://github.com/CiscoCXSecurity/enum4linux.git"
 
 # WHERE DO YOU WANT TO INSTALL IT
 INSTALL_LOCATION="enum4linux"
@@ -26,7 +26,10 @@ DEBIAN="git,perl,smbclient"
 FEDORA="git,perl,samba-client"
 
 # COMMANDS TO RUN AFTER
-AFTER_COMMANDS="cd {INSTALL_LOCATION}, wget https://labs.portcullis.co.uk/download/polenum-0.2.tar.bz2, tar -xjvf polenum-0.2.tar.bz2; rm polenum-0.2.tar.bz2, mv polenum-0.2/polenum.py /usr/bin/, rm -rf polenum-0.2"
+AFTER_COMMANDS="wget https://raw.githubusercontent.com/Wh1t3Fox/polenum/master/polenum.py -O /usr/local/bin/polenum, chmod +x /usr/local/bin/polenum"
 
 # CREATE LAUNCHER
 LAUNCHER="enum4linux"
+
+# PREREQ INSTALL MODULES NEEDED FOR THIS TOOL TO WORK PROPERLY
+TOOL_DEPEND="modules/exploitation/impacket"
