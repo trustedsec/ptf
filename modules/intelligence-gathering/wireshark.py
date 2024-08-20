d2c9dff8c25583fc3d8631a8c4abb9ac539384b5
Kamyar <139503563+GlobularOne@users.noreply.github.com>
2024-08-20 20:10:53 +0330

    Add wireshark module

---
+++ modules/intelligence-gathering/wireshark.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for wireshark
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Wireshark"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update Wireshark"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE=""
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION=""
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION=""
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="wireshark"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="wireshark"
+
+# BYPASS UPDATES
+BYPASS_UPDATE="YES"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
