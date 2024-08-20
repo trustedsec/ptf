2c1436cd496365e8362bd46b5561ebc60cb595cd
Kamyar <139503563+GlobularOne@users.noreply.github.com>
2024-08-20 20:10:44 +0330

    Add cutycapt module

---
+++ modules/intelligence-gathering/cutycapt.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for cutycapt
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Björn Höhrmann"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update Cutycapt"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE=""
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION=""
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION=""
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="cutycapt"
+
+# BYPASS UPDATES
+BYPASS_UPDATE="YES"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
